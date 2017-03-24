36a35cc46967c9bb2f41501a8aa4476e66140abe
jjenks97 <jonathan.jenkins97@gmail.com>
2017-03-24 00:52:02 -0400

    Add files via upload

---
+++ NumberFun/src/NumberFun/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NumberFun.Model;
+
+namespace NumberFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Results(string number)
+        {
+            int num;
+            if ( int.TryParse(number, out num) )
+            {
+                NumericProperties numProp = new NumericProperties(num);
+                return View(numProp);
+            }else
+            {
+                return Error();
+            }  
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ NumberFun/src/NumberFun/Model/NumericProperties.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberFun.Model
+{
+    
+    public class NumericProperties
+    {
+        public int Num { get; set; }
+        public NumericProperties(int num)
+        {
+            Num = num;
+        }
+        public bool isPrime()
+        {
+            if (Num == 1)
+            {
+                return false;
+            }
+            if (Num == 2)
+            {
+                return true;
+            }
+            int squareRoot = (int)Math.Floor(Math.Sqrt(Num));
+
+            for (int i = 2; i <= squareRoot; i++)
+            {
+                if (Num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool isPrime(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }
+            if (num == 2)
+            {
+                return true;
+            }
+            int squareRoot = (int)Math.Floor(Math.Sqrt(num));
+
+            for (int i = 2; i <= squareRoot; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool isPerfect()
+        {
+            int sum = 0;
+            for (int i = 1; i < Num; i++)
+            {
+                if (Num % i == 0 )
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == Num)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public bool isSquareful()
+        {
+            for (int i = 2; i <= Num/2; i++)
+            {
+                if (Num % i == 0)
+                {
+                    if (isPrime(i))
+                    {
+                        if (Num % (i * i) == 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool isPowerful()
+        {
+            for (int i = 2; i <= Num / 2; i++)
+            {
+                if (Num % i == 0)
+                {
+                    if (isPrime(i))
+                    {
+                        if (Num % (i * i) != 0)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        public bool isPalindrome()
+        {
+            int remainder = 0;
+            int reverse = 0;
+            int num2 = Num;
+            while (num2 > 0)
+            {
+                remainder = num2 % 10;
+                reverse = (reverse * 10) + remainder;
+                num2 = num2 / 10;
+            }
+            if (Num == reverse)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
